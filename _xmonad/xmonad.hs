da66ab2169fe7826b492cce565a554c79909450c
Loki Davison <loki.davison@gmail.com>
2012-01-20 17:42:26 +1100

    magic dmenu using command t esq window switching

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,8 +1,14 @@
 -- Loki Davison's xmonad config
 import XMonad
+import XMonad.Actions.WindowBringer
+import XMonad.Util.EZConfig
 
 main = xmonad $ defaultConfig
     { borderWidth        = 1
-    , terminal           = "/usr/bin/konsole"
+    , terminal           = "/usr/bin/urxvt -pe tabbed"
     , normalBorderColor  = "#000000"
-    , focusedBorderColor = "#cd8b00" }
+    , focusedBorderColor = "#00bb00" }
+	`additionalKeysP`
+	[ ("M-g", gotoMenu)
+    , ("M-b", bringMenu)
+	]
